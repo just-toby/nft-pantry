002937304f365f2b90c8587fa4b9b3a0da5f8806
Eddie Dugan <eddie.dugan@uniswap.org>
2023-01-26 16:17:17 -0500

    feat: render trade card on txn detail page

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -8,9 +8,11 @@ export function Header() {
   return (
     <div
       className={tw(
-        "fixed top-0 w-full p-4 justify-between flex items-center",
+        "fixed",
+        "top-0 w-full p-4 justify-between flex items-center",
+        "bg-white md:bg-transparent",
         "bg-gradient-to-b from-white to-transparent",
-        "z-50"
+        "z-50 drop-shadow-md md:drop-shadow-none"
       )}
     >
       <Link href="/">
--- pages/[txnid].tsx
+++ pages/[txnid].tsx
@@ -2,57 +2,28 @@ import "@rainbow-me/rainbowkit/styles.css";
 
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { useMemo } from "react";
-import { useNetwork, useWaitForTransaction } from "wagmi";
+import useSWR from "swr";
 
+import { NftTradeCard } from "@/components/feed/NftTradeCard";
 import { Header } from "@/components/header/Header";
+import { useGraphQLClientSdk } from "@/graphql/GraphQLContext";
 import styles from "@/styles/Home.module.css";
-import { filterNulls } from "@/util";
-import { getTxHashWithPrefix } from "@/util/addresses";
-import { ERC721TransferTopic } from "@/util/contractEvents";
 import { tw } from "@/util/tailwind";
-
-type NFTTransfer = {
-  from: string;
-  to: string;
-  tokenId: string;
-  contractAddress: string;
-};
 
 export default function Home() {
   const router = useRouter();
   const { txnid } = router.query;
+  const sdk = useGraphQLClientSdk();
 
-  const { chain } = useNetwork();
-  const { data, error } = useWaitForTransaction({
-    chainId: chain?.id,
-    hash: getTxHashWithPrefix(txnid as string),
-  });
-
-  const transfers: NFTTransfer[] = useMemo(() => {
-    return filterNulls(
-      data?.logs.map((log) => {
-        if (log.topics[0] === ERC721TransferTopic && log.topics.length === 4) {
-          const from = log.topics[1];
-          const to = log.topics[2];
-          const tokenId = log.topics[3];
-          const contractAddress = log.address;
-          return {
-            from,
-            to,
-            tokenId,
-            contractAddress,
-          };
-        }
-        return null; // not an ERC721 transfer event
-      }) ?? []
-    );
-  }, [data?.logs]);
-
-  // if (error) {
-  //   // todo: make an error state
-  //   return <div>error</div>;
-  // }
+  const { data: transaction } = useSWR(
+    "TransactionDetails" + txnid,
+    async () => {
+      const result = await sdk?.FeedQuery({
+        transactionHash: txnid as string,
+      });
+      return result?.data?.feed?.[0];
+    }
+  );
 
   return (
     <>
@@ -62,14 +33,12 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={tw(styles.main, "text-white")}>
+      <main className={tw(styles.main, "text-primary")}>
         <Header />
 
-        <div className="h-full w-full flex items-center justify-center text-4xl">
-          {txnid}
+        <div className="mt-20 max-w-xl">
+          {transaction && <NftTradeCard trade={transaction} />}
         </div>
-        {/* feed */}
-        {/* todo */}
       </main>
     </>
   );
